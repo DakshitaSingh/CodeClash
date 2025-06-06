126626d40c6d35b842f0586523d6ad2454981ea5
Dakshita Singh <dakshitasingh2222@gmail.com>
2025-06-06 11:04:26 +0530

    fixed

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/AmazonHackon/", // ✅ Set to your GitHub repo name
-});
+})
